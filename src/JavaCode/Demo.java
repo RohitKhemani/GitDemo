d9fde3fc3eca41345bba7f553956d72bd8130999
RohitKhemani <rohitkhemani2@gmail.com>
2020-07-26 12:06:30 +0530

    Adding new file

---
+++ src/JavaCode/Demo.java
@@ -0,0 +1,15 @@
+package JavaCode;
+import org.apache.log4j.*;
+
+public class Demo {
+	
+	private static Logger log = LogManager.getLogger(Demo.class.getName());
+
+	public static void main(String[] args) {
+		
+		
+		log.info("Application Launched");
+
+	}
+
+}
